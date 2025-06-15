0540bb3ba6ed23700c2742c69f1ec2c8acc28e2b
Gasym Valiyev <outnesss@yandex.ru>
2025-06-15 08:28:40 +0400

    Removed DataTalks-AI

--- DataTalks-AI/urls(1).py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('query/', views.handle_query, name='handle_query'),
-    path('', views.home, name='home'),  # Serve home.html at root
-]
-
