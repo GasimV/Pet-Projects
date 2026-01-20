f500a387092dcf7a8c136ee66d165e4f4f22ee58
Gasym Valiyev <outnesss@yandex.ru>
2026-01-21 01:27:17 +0400

    ...

--- Speech Processing/Django-Demo/urls(1).py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('process_audio/', views.process_audio, name='process_audio'),
-]
