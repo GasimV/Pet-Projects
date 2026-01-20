5ec177e9bc0351dae088de54424dc336ec1929c0
Gasym Valiyev <outnesss@yandex.ru>
2026-01-21 01:47:13 +0400

    ...

--- vCard-Business-QR-Manager/vcard-business-qr-manager/backend/src/templates.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-#include "nlohmann/json.hpp"
-
-using json = nlohmann::json;
-
-std::string generateBusinessPageLandingPage(const json& businesspage, const json& design, const std::string& shortCode);
-std::string generateVCardLandingPage(const json& vcard, const json& design, const std::string& shortCode);
-std::string generateVCF(const json& vcard);
